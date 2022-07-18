b519061242eb9245f5729f4d915c33a94f497318
Sam Edwards <samdwar@amazon.com>
2022-07-19 08:59:32 +1000

    added a check to see if sdocker is already executable

--- setup.sh
+++ setup.sh
@@ -9,10 +9,16 @@ then
     if [[ $EUID != 0 ]]
     then
         sudo ln -s $PWD/src/sdocker/sdocker /usr/bin/sdocker
-        sudo chmod +x $PWD/src/sdocker/sdocker
+        if [[ -x  "$PWD/src/sdocker/sdocker" ]]
+        then
+            sudo chmod +x $PWD/src/sdocker/sdocker
+        fi
     else
         ln -s $PWD/src/sdocker/sdocker /usr/bin/sdocker
-        chmod +x $PWD/src/sdocker/sdocker
+        if [[ -x  "$PWD/src/sdocker/sdocker" ]]
+        then
+            chmod +x $PWD/src/sdocker/sdocker
+        fi
     fi
 fi
 
